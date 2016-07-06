652d875f59dd6ba1317daa1c8f1e564f524f0123
fidelscodes <fidelscodes@gmail.com>
2016-07-06 10:36:13 -0400

    Update with solution

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require_relative "./user.rb"
 
+class Student < User
+  def initialize
+    @knowledge = Array.new
+  end
+
+  def learn(knowledge_str)
+    @knowledge << knowledge_str
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
