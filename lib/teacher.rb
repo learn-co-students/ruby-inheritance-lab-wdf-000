cac6f4c1aa38286f0a8db28e1e1f3939c1b8ad6c
hafizrahman1 <hafiz.bd2009@gmail.com>
2016-07-23 18:20:55 -0400

    UpdatedDone.

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -3,9 +3,6 @@ require 'pry'
 class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javacsript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance varaible", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
-  
-  def initialize
-  end
 
   def teach
     KNOWLEDGE[rand(0.7)]
