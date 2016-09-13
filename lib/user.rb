914a09995c25c21efce79cff4fd5416be6ed6078
Keo  Bun <keonilrath@gmail.com>
2016-09-13 02:23:36 +0000

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,14 @@
+class User
+
+  attr_accessor :first_name, :last_name, :new
+
+  def intialize(first_name, last_name)
+    @first_name = name
+    @last_name = name
+    @new = new_student
+  end
+
+  def new_student
+    @new = "#{first_name} #{last_name}"
+  end
+end
